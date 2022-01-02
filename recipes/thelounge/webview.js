a447d95929949e57b8525d78ae9c19a65a4d8f61
Brian Kendall <brian@briankendall.net>
2022-01-02 16:07:59 -0500

    [thelounge] Calculate direct message badge as tally of highlighted messages

--- recipes/thelounge/webview.js
+++ recipes/thelounge/webview.js
@@ -1,10 +1,18 @@
 module.exports = Ferdi => {
   const getMessages = () => {
+    let direct = 0;
+    var directElements = document.querySelectorAll('.badge.highlight');
+
+    for (const directElement of directElements) {
+      if (directElement.textContent.length > 0) {
+        direct += Ferdi.safeParseInt(directElement.textContent);
+      }
+    }
+
+    let indirect = 0;
     const indirectElements = document.querySelectorAll(
       '.badge:not(.highlight)',
     );
-    const direct = document.querySelectorAll('.badge.highlight').length;
-    let indirect = 0;
     for (const indirectElement of indirectElements) {
       if (indirectElement.textContent.length > 0) {
         indirect++;
